834a666174b0ea4ce35e3d5e021056aebe629ddd
mmaciasjr <65560980+mmaciasjr@users.noreply.github.com>
2020-08-01 23:54:41 -0700

    Add files via upload

--- passwordGen.js
+++ passwordGen.js
@@ -1,6 +1,5 @@
-// DOM
 const resultEl = document.getElementById('result');
-const lenthEl = document.getElementById('length');
+const lengthEl = document.getElementById('length');
 const uppercaseEl = document.getElementById('uppercase');
 const lowercaseEl = document.getElementById('lowercase');
 const numbersEl = document.getElementById('numbers');
@@ -31,9 +30,8 @@ clipboard.addEventListener('click', () => {
   alert('Password copied to clipboard');
 });
 
-//Generate Event Listener
-generateEl.addEventListener('click', () => {
-  const lengthEl = +length.value;
+generate.addEventListener('click', () => {
+  const length = +lengthEl.value;
   const hasLower = lowercaseEl.checked;
   const hasUpper = uppercaseEl.checked;
   const hasNumber = numbersEl.checked;
@@ -48,58 +46,31 @@ generateEl.addEventListener('click', () => {
   );
 });
 
-// Copy passsword to clipboard
-clipboard.addEventListener('click', () => {
-  const textarea = document.createElement('textarea');
-  const password = resultEl.innerText;
-
-  if (!password) {
-    return;
-  }
-
-  textarea.value = password;
-  document.body.appendChild(textarea);
-  textarea.select();
-  document.execCommand('copy');
-  textarea.remove();
-  alert('Password copied to clipboard');
-});
-
-//Generate password function
 function generatePassword(lower, upper, number, symbol, length) {
-  //1. Init pw var
-  //2. Filter out all unchecked types. Dont want to include uppercase.
-  //3. Loop over length and call a generator function for each type.
-  //4. Add a final password to the password variable and return
-
-  let generatePassword = '';
-
+  let generatedPassword = '';
   const typesCount = lower + upper + number + symbol;
-
-  const typesArr = [{ lower }, { upper }, { numbers }, { symbol }].filter(
+  const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(
     (item) => Object.values(item)[0]
   );
 
+  // Doesn't have a selected type
   if (typesCount === 0) {
     return '';
   }
 
+  // create a loop
   for (let i = 0; i < length; i += typesCount) {
     typesArr.forEach((type) => {
       const funcName = Object.keys(type)[0];
-
       generatedPassword += randomFunc[funcName]();
     });
   }
-  //can add console.log instead of const final Password to see in web browser console.
-  // .slice is used to create a password that is less than the intended minimum requirements. Just some fun code.
-  const finalPassword = generatePassword.slice(0, length);
+
+  const finalPassword = generatedPassword.slice(0, length);
 
   return finalPassword;
 }
-
-// Generator functions used charcode for symbols - http://www.net-comber.com/charset.html
-
+//Math.floor is used to round down to nearest whole number
 function getRandomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
@@ -109,10 +80,10 @@ function getRandomUpper() {
 }
 
 function getRandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+  return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
 
 function getRandomSymbol() {
-  const symbols = '!@#$%^&*()+={}[]<>~`';
+  const symbols = '!@#$%^&*(){}[]=<>/,.';
   return symbols[Math.floor(Math.random() * symbols.length)];
 }
